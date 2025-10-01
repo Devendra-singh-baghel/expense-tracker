8c40dde58bc0d2eec48a538a4fb2e84c08b2bbfe
Devendra Singh Baghel <imdbsingh4u@gmail.com>
2025-10-01 10:58:30 +0530

    Create custom fields and implement form validation

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Input from './Input'
+import Select from './Select'
 
 export default function ExpenseForm({ setExpenses }) {
+
 
     //React support one way data binding.
     //Using useState we create controlled components
@@ -12,11 +15,49 @@ export default function ExpenseForm({ setExpenses }) {
         amount: '',
     })
 
+    const [errors, setErrors] = useState({})
+
+    const validationConfig = {
+        title: [
+            { required: true, message: 'Please enter title' },
+            { minLength: 5, message: 'Title should be at least 5 characters long' },
+        ],
+        category: [{ required: true, message: 'Please select a category' }],
+        amount: [{ required: true, message: 'Please enter an amount' }],
+    }
+
+    const validate = (formData) => {
+        const errorsData = {}
+
+        Object.entries(formData).forEach(([key, value]) => {
+            validationConfig[key].some((rule) => {
+                if (rule.required && !value) {
+                    errorsData[key] = rule.message
+                    return true
+                }
+
+                if (rule.minLength && value.length < 5) {
+                    errorsData[key] = rule.message
+                    return true
+                }
+            })
+        })
+
+        setErrors(errorsData)
+        return errorsData
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        //crypto.randomUUID() generate random id 
-        setExpenses((prevState) => [...prevState, { ...expense, id: crypto.randomUUID() }])
+
+        const validateResult = validate(expense)
+
+        if (Object.keys(validateResult).length) return
+
+        setExpenses((prevState) => [
+            ...prevState,
+            { ...expense, id: crypto.randomUUID() },
+        ])
         setExpense({
             title: '',
             category: '',
@@ -24,60 +65,46 @@ export default function ExpenseForm({ setExpenses }) {
         })
     }
 
-    /*
-    The `handleChange` function updates the `expense` state with the new value based on the input
-    field name.
-     */
+
+
+    //The `handleChange` function updates the `expense` state with the new value based on the input field name.
     const handleChange = (e) => {
-        const { name, value } = e.target;
+        const { name, value } = e.target
         setExpense((prevState) => ({
             ...prevState,
             [name]: value,
         }))
+        setErrors({})
     }
-
 
     return (
         <form className="expense-form" onSubmit={handleSubmit}>
-            <div className="input-container">
-                <label htmlFor="title">Title</label>
-                <input
-                    id="title"
-                    name="title"
-                    value={expense.title}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div className="input-container">
-                <label htmlFor="category">Category</label>
-                <select
-                    id="category"
-                    name="category"
-                    value={expense.category}
-                    onChange={handleChange}
-                    required
-                >
-                    <option value="" hidden>
-                        Select Category
-                    </option>
-                    <option value="grocery">Grocery</option>
-                    <option value="clothes">Clothes</option>
-                    <option value="bills">Bills</option>
-                    <option value="education">Education</option>
-                    <option value="medicine">Medicine</option>
-                </select>
-            </div>
-            <div className="input-container">
-                <label htmlFor="amount">Amount</label>
-                <input
-                    id="amount"
-                    name="amount"
-                    value={expense.amount}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
+            <Input
+                label="Title"
+                id="title"
+                name="title"
+                value={expense.title}
+                onChange={handleChange}
+                error={errors.title}
+            />
+            <Select
+                label="Category"
+                id="category"
+                name="category"
+                value={expense.category}
+                onChange={handleChange}
+                options={['Grocery', 'Clothes', 'Bills', 'Education', 'Medicine']}
+                defaultOption="Select Category"
+                error={errors.category}
+            />
+            <Input
+                label="Amount"
+                id="amount"
+                name="amount"
+                value={expense.amount}
+                onChange={handleChange}
+                error={errors.amount}
+            />
             <button className="add-btn">Add</button>
         </form>
     )
